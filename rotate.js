d4a08efae21f85cf3c8d24b738e2585310f334b7
Brian Green <briangreenery@gmail.com>
2015-01-24 02:26:32 -0800

    simplify

--- rotate.js
+++ rotate.js
@@ -1,35 +1,30 @@
 var fs = require('fs');
 
-function rotatedPath(path, num) {
+function makePath(path, num) {
   return path + '.' + num;
 }
 
 function rotate(path, count, cb) {
   function shift(num) {
-    var src, dst;
-
     if (num === 0) {
       return cb(null);
     }
 
-    src = rotatedPath(path, num - 1);
-    dst = rotatedPath(path, num);
-
-    fs.rename(src, dst, function(err) {
+    fs.rename(makePath(path, num - 1), makePath(path, num), function(err) {
       if (err && err.code !== 'ENOENT') {
-        cb(err);
-      } else {
-        shift(num - 1);
+        return cb(err);
       }
+      
+      shift(num - 1);
     });
   }
 
-  fs.unlink(rotatedPath(path, count - 1), function(err) {
+  fs.unlink(makePath(path, count - 1), function(err) {
     if (err && err.code !== 'ENOENT') {
-      cb(err);
-    } else {
-      shift(count - 1);
+      return cb(err);
     }
+    
+    shift(count - 1);
   });
 }
 
